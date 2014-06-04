641efb44609be04c88246f90b7e985c36368cbd3
Addy Osmani <addyosmani@gmail.com>
2014-06-04 21:52:08 +0100

    Drop default key and use the nokey option.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp');
 var psi = require('psi');
 var site = 'http://www.html5rocks.com';
-var key = 'AIzaSyCHBBOqcgSVUC_shyK6BEAKOZZoBpJCF6g';
+var key = '';
 
 // TODO FOR YOU:
 // Register for own key over at https://console.developers.google.com/
 // Following this guide: 
 // https://developers.google.com/speed/docs/insights/v1/getting_started
-// You shouldn't use the above key for your own builds.
 
 gulp.task('mobile', function () {
 	return psi({
-	    key: key,
+		// key: key
+	    nokey: 'true',
 	    url: site,
 	    strategy: 'mobile',
 	});
@@ -19,7 +19,8 @@ gulp.task('mobile', function () {
 
 gulp.task('desktop', function () {
 	return psi({
-	    key: key,
+		nokey: 'true',
+	    // key: key,
 	    url: site,
 	    strategy: 'desktop',
 	});
